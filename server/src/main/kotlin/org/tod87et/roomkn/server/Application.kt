01ef66c38d118c6526bcb2825f260dd7931df3c0
Makar Selivanov <makar.selivanov@gmail.com>
2023-10-08 14:02:34 +0300

    add configureCORS and move code from Application.kt

--- server/src/main/kotlin/org/tod87et/roomkn/server/Application.kt
+++ server/src/main/kotlin/org/tod87et/roomkn/server/Application.kt
@@ -1,22 +1,12 @@
 package org.tod87et.roomkn.server
 
-import io.ktor.http.*
-import io.ktor.server.application.*
-import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.application.Application
+import org.tod87et.roomkn.server.plugins.configureCORS
 import org.tod87et.roomkn.server.plugins.configureRouting
 import org.tod87et.roomkn.server.plugins.configureSerialization
 
 fun Application.module() {
-    install(CORS) {
-        anyHost()
-        allowHeader("X-Requested-With")
-        allowHeader(HttpHeaders.Origin)
-        allowHeader(HttpHeaders.ContentType)
-        allowHeader(HttpHeaders.AccessControlAllowOrigin)
-        allowHeader(HttpHeaders.AccessControlAllowMethods)
-        allowHeader(HttpHeaders.AccessControlAllowHeaders)
-        allowCredentials = true
-    }
+    configureCORS()
     configureRouting()
     configureSerialization()
 }
---
+++ server/src/main/kotlin/org/tod87et/roomkn/server/plugins/Cors.kt
@@ -0,0 +1,19 @@
+package org.tod87et.roomkn.server.plugins
+
+import io.ktor.http.HttpHeaders
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.cors.routing.CORS
+
+fun Application.configureCORS() {
+    install(CORS) {
+        anyHost()
+        allowHeader("X-Requested-With")
+        allowHeader(HttpHeaders.Origin)
+        allowHeader(HttpHeaders.ContentType)
+        allowHeader(HttpHeaders.AccessControlAllowOrigin)
+        allowHeader(HttpHeaders.AccessControlAllowMethods)
+        allowHeader(HttpHeaders.AccessControlAllowHeaders)
+        allowCredentials = true
+    }
+}
