dc979261fcc97929bb46aed856fedba8040f1834
Makar Selivanov <makar.selivanov@gmail.com>
2023-10-15 17:04:28 +0300

    add createRoom in interface + model NewRoomInfo

--- server/src/main/kotlin/org/tod87et/roomkn/server/database/Database.kt
+++ server/src/main/kotlin/org/tod87et/roomkn/server/database/Database.kt
@@ -1,5 +1,6 @@
 package org.tod87et.roomkn.server.database
 
+import org.tod87et.roomkn.server.models.NewRoomInfo
 import org.tod87et.roomkn.server.models.Reservation
 import org.tod87et.roomkn.server.models.RoomInfo
 import org.tod87et.roomkn.server.models.ShortRoomInfo
@@ -10,10 +11,12 @@ import org.tod87et.roomkn.server.models.UserInfo
 interface Database {
     fun getRooms(): Result<List<ShortRoomInfo>>
     fun getRoom(roomId: Int): Result<RoomInfo>
+    fun createRoom(roomInfo: NewRoomInfo): Result<RoomInfo>
     fun getRoomReservations(roomId: Int): Result<List<Reservation>>
     fun createReservation(reserve: UnregisteredReservation): Result<Reservation>
     fun getUsers(): Result<List<ShortUserInfo>>
     fun getUser(userId: Int): Result<UserInfo>
+
 
     /**
      * Clear database for TEST/DEBUG purpose
