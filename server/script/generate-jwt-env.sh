f107f25d722e43392223b9a6bbc431727b26a198
firelion9 <develop.firelion@gmail.com>
2023-10-24 21:09:44 +0300

    Fix issues with `executable` permissions in secret generator

--- server/script/generate-jwt-env.sh
+++ server/script/generate-jwt-env.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 mkdir local
 
 echo "#!/bin/sh" > local/jwtRS256.env
@@ -10,5 +12,7 @@ echo '' >> local/jwtRS256.env
 echo -n 'export PEPPER=' >> local/jwtRS256.env
 head --bytes 64 /dev/urandom | base64 -w 0 >> local/jwtRS256.env
 echo '' >> local/jwtRS256.env
+
+chmod +x local/jwtRS256.env
 
 echo "Environment script has been written to $(pwd)/local/jwtRS256.env"
