e9f31d6b0e8401912e83838205fc50ed40fdcffd
Pavel Balay <pavel.balai@gmail.com>
2023-10-30 17:02:53 +0300

    Update databse api

--- server/src/main/kotlin/org/tod87et/roomkn/server/models/users/RegistrationUserInfo.kt
+++ server/src/main/kotlin/org/tod87et/roomkn/server/models/users/RegistrationUserInfo.kt
@@ -1,9 +1,12 @@
 package org.tod87et.roomkn.server.models.users
 
+import org.tod87et.roomkn.server.models.permissions.UserPermission
+
 /** For internal use only. **Not** serializable */
 class RegistrationUserInfo(
     val username: String,
     val email: String,
     val salt: ByteArray,
     val passwordHash: ByteArray,
+    val permissions: List<UserPermission> = listOf()
 )
