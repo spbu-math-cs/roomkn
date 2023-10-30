0ea876e8db72834dca5ff45874a4f22e57ce96eb
firelion9 <develop.firelion@gmail.com>
2023-10-30 22:40:44 +0300

    Remove user_id completely

--- server/src/main/kotlin/org/tod87et/roomkn/server/models/reservations/UnregisteredReservation.kt
+++ server/src/main/kotlin/org/tod87et/roomkn/server/models/reservations/UnregisteredReservation.kt
@@ -1,13 +1,12 @@
 package org.tod87et.roomkn.server.models.reservations
 
 import kotlinx.datetime.Instant
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
 
-@Serializable
 data class UnregisteredReservation(
-    @SerialName("user_id") val userId: Int = -1,
-    @SerialName("room_id") val roomId: Int,
+    val userId: Int,
+    val roomId: Int,
     val from: Instant,
     val until: Instant,
 )
+
+fun ReservationRequest.toUnregisteredReservation(userId: Int) = UnregisteredReservation(userId, roomId, from, until)
--- server/src/main/kotlin/org/tod87et/roomkn/server/routing/Reserve.kt
+++ server/src/main/kotlin/org/tod87et/roomkn/server/routing/Reserve.kt
@@ -2,30 +2,33 @@ package org.tod87et.roomkn.server.routing
 
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
+import io.ktor.server.auth.authenticate
 import io.ktor.server.auth.principal
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
 import org.tod87et.roomkn.server.auth.AuthSession
+import org.tod87et.roomkn.server.auth.AuthenticationProvider
 import org.tod87et.roomkn.server.database.DatabaseFactory.database
-import org.tod87et.roomkn.server.models.reservations.UnregisteredReservation
+import org.tod87et.roomkn.server.models.reservations.ReservationRequest
+import org.tod87et.roomkn.server.models.reservations.toUnregisteredReservation
 
 fun Route.reserveRouting() {
-    post("/reserve") { unregisteredReservation: UnregisteredReservation ->
-        val userId = runCatching {
-            call.principal<AuthSession>()?.userId ?: unregisteredReservation.userId
-        }.getOrElse { unregisteredReservation.userId }
+    authenticate(AuthenticationProvider.SESSION) {
+        post("/reserve") { body: ReservationRequest ->
+            val userId = call.principal<AuthSession>()!!.userId
 
-        val result = database.createReservation(unregisteredReservation.copy(userId = userId))
-        result.onSuccess {
-            call.respond(HttpStatusCode.Created, it)
-        }
-        result.onFailure {
-            return@post call.respondText(
-                "Failed to add reservation: conflict with other reservations",
-                status = HttpStatusCode.Conflict
-            )
+            val result = database.createReservation(body.toUnregisteredReservation(userId))
+            result.onSuccess {
+                call.respond(HttpStatusCode.Created, it)
+            }
+            result.onFailure {
+                return@post call.respondText(
+                    "Failed to add reservation: conflict with other reservations",
+                    status = HttpStatusCode.Conflict
+                )
+            }
         }
     }
 }
