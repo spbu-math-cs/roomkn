bffb9dec0cc71a54aa7fbee9f4814bd60c8b3363
firelion9 <develop.firelion@gmail.com>
2023-11-18 14:21:41 +0300

    Add initialization logging, fix dev startup

--- server/src/main/kotlin/org/tod87et/roomkn/server/Application.kt
+++ server/src/main/kotlin/org/tod87et/roomkn/server/Application.kt
@@ -1,6 +1,7 @@
 package org.tod87et.roomkn.server
 
 import io.ktor.server.application.Application
+import io.ktor.util.logging.KtorSimpleLogger
 import org.tod87et.roomkn.server.plugins.configureAuthentication
 import org.tod87et.roomkn.server.plugins.configureCORS
 import org.tod87et.roomkn.server.plugins.configureCleanup
@@ -9,9 +10,13 @@ import org.tod87et.roomkn.server.plugins.configureSerialization
 
 @Suppress("UNUSED") // Used through kTor modules in application.conf
 fun Application.module() {
+    val logger = KtorSimpleLogger("RooMKN")
+
+    logger.info("Initializing RooMKN main module...")
     configureAuthentication()
     configureCORS()
     configureRouting()
     configureSerialization()
     configureCleanup()
+    logger.info("RooMKN main module has been initialized")
 }
