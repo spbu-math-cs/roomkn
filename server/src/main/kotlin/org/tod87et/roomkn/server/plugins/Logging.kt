75bbe5f71d307f92cc619fabdaa8ac1e7dcfbe68
Makar Selivanov <makar.selivanov@gmail.com>
2023-11-25 02:44:40 +0300

    add CallLogging

---
+++ server/src/main/kotlin/org/tod87et/roomkn/server/plugins/Logging.kt
@@ -0,0 +1,23 @@
+package org.tod87et.roomkn.server.plugins
+
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.callloging.CallLogging
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.request.httpMethod
+import io.ktor.server.request.uri
+import org.slf4j.event.Level
+
+fun Application.configureLogging() {
+    install(CallLogging) {
+        level = Level.INFO
+        format { call ->
+            val status = call.response.status()
+            val httpMethod = call.request.httpMethod.value
+            val userAgent = call.request.headers["User-Agent"]
+            val route = call.request.uri
+            "Status: $status, HTTP method: $httpMethod, User agent: $userAgent, Route: $route"
+        }
+    }
+}
