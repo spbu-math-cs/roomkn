2d12a0d14d5b83f530b2d5f2be02484cf37a7720
Павел Ральников <112890673+PaulRalnikov@users.noreply.github.com>
2023-12-13 15:46:15 +0300

    Timeline in room list (#271)
    * Added timeline for room list
    
    * Added timeline to room list (doesn't work now)
    
    * Added timeline in room list!!!!!!
    
    * Added dividers in timeline
    
    * Added react import
    
    * Make timeline and room in one row

--- client/src/pages/List.js
+++ client/src/pages/List.js
@@ -2,17 +2,124 @@ import './List.css'
 import ContentWrapper from "../components/Content"
 import useSomeAPI from "../api/FakeAPI"
 import React, {useEffect, useState} from "react";
-import {Divider, List, ListItemButton, ListItemText} from "@mui/material";
+import {
+    Box,
+    Grid,
+    ListItemButton,
+    Stack,
+    TextField,
+} from "@mui/material";
+import {fromAPITime} from "../api/API";
+import TimelineForRoomList from "../components/TimelineForRoomList";
 
-function RoomRow({room}) {
+function dateFormat(date, format = "yyyy-mm-dd") {
+    var mlz = ""
+    if (date.getMonth() + 1 < 10) mlz = "0"
+    var dlz = ""
+    if (date.getDate() < 10) dlz = "0"
+    const map = {
+        mm: mlz + (date.getMonth() + 1),
+        dd: dlz + date.getDate(),
+        yyyy: date.getFullYear(),
+        // yy: date.getFullYear().toString().slice(-2)
+    }
+
+    return format.replace(/mm|dd|yyyy/gi, matched => map[matched])
+}
+
+function getTodayDate(format = "yyyy-mm-dd") {
+    const date = new Date()
+
+    return dateFormat(date, format)
+}
+
+function GetReservationsInSegment(room_id, dateFrom, dateUntil) {
+
+    let {triggerFetch} = useSomeAPI('/api/v0/rooms/' + room_id + '/reservations', null, 'GET', ReservationsCallback)
+
+    let [reservs, setReservs] = useState({
+        reservations: null,
+        triggerGetReservations: triggerFetch
+    })
+
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(() => triggerFetch(), [dateFrom, dateUntil])
+
+    function ReservationsCallback(result, statusCode) {
+        console.log("all reservations for room " + room_id + " are (status code: " + statusCode + " ):")
+        if (statusCode === 200 && result != null) {
+            setReservs({
+                reservations: result.filter((reservation) => (
+                    fromAPITime(reservation.from).date <= dateUntil &&
+                    fromAPITime(reservation.until).date >= dateFrom
+                )),
+                triggerGetReservations: triggerFetch
+            })
+        } else {
+            setReservs({
+                reservations: null,
+                triggerGetReservations: triggerFetch
+            })
+        }
+    }
+
+    return reservs
+}
+
+function updateDate(date, diff) {
+    const new_date = new Date(date)
+    new_date.setDate(new_date.getDate() + diff)
+    return new_date
+}
+
+function TimelineForRoom({room, fromDate, untilDate}) {
+    const {reservations} = GetReservationsInSegment(room.id, fromDate, untilDate)
+
+    let realFromDate = new Date(fromDate)
+    realFromDate.setHours(0, 0)
+
+    let realUntilDate = new Date(untilDate)
+    realUntilDate.setHours(0, 0)
+    realUntilDate = updateDate(realUntilDate, +1)
+
+    return (
+        <TimelineForRoomList reservations = {reservations} fromTimelineDate={realFromDate} untilTimelineDate={realUntilDate}/>
+    )
+}
+
+function DateSelect({setFromDate, setUntilDate}) {
+
+    const date = getTodayDate()
+
+    return (
+        <Stack direction="row" spacing={1}>
+            {/*<DateTimePicker>*/}
+
+            {/*</DateTimePicker>*/}
+            <TextField id="date" label="From" type="date" defaultValue={date} onChange={(e) => {setFromDate(e.target.value)}}/>
+            <TextField id="date" label="Until" type="date" defaultValue={date} onChange={(e) => {setUntilDate(e.target.value)}}/>
+        </Stack>
+    )
+}
+
+function RoomRow({room, from, until}) {
 
     const link = "/room/" + String(room.id)
 
     return (
-        <ListItemButton href={link} data-test-id={"link-" + room.id}>
-            <ListItemText primary={room.name} secondary={room.description}/>
-            <Divider/>
-        </ListItemButton>
+        <Grid item>
+            <ListItemButton href={link} data-test-id={"link-" + room.id}>
+            <Grid container item alignItems="center">
+                <Grid item xs = {1}>
+                    <Box fontSize={20}> {room.name} </Box>
+                </Grid>
+                <Grid item xs = {1}>
+                    <TimelineForRoom room={room} fromDate={from} untilDate={until}/>
+                </Grid>
+            </Grid>
+            </ListItemButton>
+        </Grid>
+
     );
 }
 
@@ -20,16 +127,19 @@ function RoomList() {
 
     let [draw_list, setDrawList] = useState([])
 
+    const today = getTodayDate()
+
+    const [from, setFrom] = useState(today)
+    const [until, setUntil] = useState(today)
+
+    const [roomList, setRoomList] = useState([])
+
     function my_callback(result, statusCode) {
         console.log("result: " + result)
         console.log("statusCode:"  + statusCode)
         if (statusCode === 200 && result != null) {
+            setRoomList(result);
             console.log(result)
-            const new_draw_list = []
-            result.forEach((room) => {
-                new_draw_list.push(<RoomRow room={room} key={room.id}/>)
-            })
-            setDrawList(new_draw_list)
         }
     }
 
@@ -37,11 +147,23 @@ function RoomList() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect(() => triggerFetch(), [])
 
+    useEffect(() => {
+        const new_draw_list = []
+        roomList.forEach((room) => {
+            new_draw_list.push(<RoomRow room={room} from={from} until={until}/>)
+        })
+        setDrawList(new_draw_list)
+    }, [from, until, roomList]);
+
     return (
         <ContentWrapper page_name="Classrooms">
-            <List>
-                {draw_list}
-            </List>
+            <Stack direction = "column">
+                <DateSelect setFromDate={setFrom} setUntilDate={setUntil}/>
+                <Grid container spacing = {3}>
+                    {draw_list}
+                </Grid>
+            </Stack>
+
         </ContentWrapper>
     );
 }
